6f2ec2eee9f1ccaeabd7285539a9dd37e219edc5
Semicolon0 <nikolaihansen05@gmail.com>
2025-12-12 00:12:29 +0100

    Add 'Still Alive' audio and update styles
    Added still_alive.mp3 to assets and included commented-out logic in script.js for switching songs. Improved style.css with typo fix, adjusted padding, margins, and z-index for better layout. Minor HTML formatting cleanup.

--- script.js
+++ script.js
@@ -1,7 +1,27 @@
 //      RADIO
 const song = document.getElementById("radio_song")
 const radio = document.getElementById("radio")
+
 song.volume = 0.4;
+
+
+//      SONG CHANGE bruges måske senere
+/*
+const song_change = document.getElementById("song_change"); //måske ændre navnet på id, hvis det ikke passer
+still_alive = false;
+
+song_change.addEventListener('click', function(){
+    if (!still_alive) {
+        song.src = "/assets/sounds/still_alive.mp3";
+        still_alive = true
+    } else {
+        song.src = "/assets/sounds/portal_radio_loop.mp3";
+        still_alive = false
+    }
+    radio.src = "/assets/images/radio_off.png"
+
+})
+*/
 
 radio.addEventListener('click', function(){
     if (song.paused){
@@ -12,3 +32,4 @@ radio.addEventListener('click', function(){
         radio.src = "/assets/images/radio_off.png";
     }
 })
+
