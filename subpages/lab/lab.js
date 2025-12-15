d46dbdc748cf51ee832f2030d8629f91c00a5131
Semicolon0 <nikolaihansen05@gmail.com>
2025-12-15 01:53:51 +0100

    Lab mostly done
    Added cursor pointer style to concept art images for better UX. Updated HTML to include left/right alignment classes for concept art sections. In JavaScript, enabled playing GLaDOS audio on image click and added a click event to redirect from the portal element to the homepage.

--- subpages/lab/lab.js
+++ subpages/lab/lab.js
@@ -1,3 +1,5 @@
+
+//      GLaDOS VO
 const glados_concept = document.getElementById("glados_concept");
 const glados_vo = document.getElementById("glados_vo");
 
@@ -5,4 +7,11 @@ glados_vo.volume = .3;
 
 glados_concept.addEventListener('click', function() {
     glados_vo.play();
+})
+
+//      PORTAL REDIRECT
+const portal = document.getElementById("portal_redir");
+
+portal.addEventListener('click', function(){
+    window.location.href = "/index.html";
 })
